fe8e37f7c8be3b8cbd74b349b86a0a052333217c
Mike Chen <ccmikechen@gmail.com>
2019-08-15 22:36:46 +0800

    Initial project

---
+++ lib/videx.ex
@@ -0,0 +1,18 @@
+defmodule Videx do
+  @moduledoc """
+  Documentation for Videx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Videx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/videx_test.exs
@@ -0,0 +1,8 @@
+defmodule VidexTest do
+  use ExUnit.Case
+  doctest Videx
+
+  test "greets the world" do
+    assert Videx.hello() == :world
+  end
+end
